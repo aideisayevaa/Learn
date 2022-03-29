fd1d272bce28f9767a1e4c19dc38fb3664662ce8
aideisayevaa <aideisayeva3@gmail.com>
2022-03-29 19:34:20 +0400

    filter js tamamlandi

--- E-Commerce Website/js/custom.js
+++ E-Commerce Website/js/custom.js
@@ -9,6 +9,7 @@ jQuery(document).ready(function($) {
     var fsOverlay = $('.fs_menu_overlay');
 
     initFavorite();
+    initIsotopeFiltering();
 
 
 
@@ -35,5 +36,29 @@ jQuery(document).ready(function($) {
             })
         }
     }
-})
 
+
+    //filter
+
+    function initIsotopeFiltering() {
+        if ($('.grid_sorting_button').length) {
+            $('.grid_sorting_button').click(function() {
+                $('.grid_sorting_button.active').removeClass('active');
+                $(this).addClass('active');
+
+                var selector = $(this).attr('data-filter');
+                $('.product-grid').isotope({
+                    filter: selector,
+                    animationOptions: {
+                        duration: 750,
+                        easing: 'linear',
+                        queue: false
+                    }
+                });
+                return false;
+            })
+        }
+    }
+
+
+})
