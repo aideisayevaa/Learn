673f29d23462c5bf0f47073badc2c52ffcf4a188
aideisayevaa <aideisayeva3@gmail.com>
2022-03-29 13:40:26 +0400

    filter css kodlarinin bir hissesi yazildi

---
+++ E-Commerce Website/js/custom.js
@@ -0,0 +1,14 @@
+// coding with nick
+
+// Js Documents
+
+// Table of contyent
+// 1.  vars and inits
+// 2.  Inits Menu
+// 3.  Init Timer
+// 4.  Init Favorite
+// 5.  Init Isotope Filtering
+// 6.  Init Slider
+
+
+
