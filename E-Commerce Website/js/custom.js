1f6b837c81987d07ef1e6bef997fdc116592fe5d
aideisayevaa <aideisayeva3@gmail.com>
2022-03-29 19:22:54 +0400

    favorite hisse ucun js yazildi

--- E-Commerce Website/js/custom.js
+++ E-Commerce Website/js/custom.js
@@ -1,14 +1,39 @@
-// coding with nick
+jQuery(document).ready(function($) {
+    "user strict";
 
-// Js Documents
+    var mainSlider = $('.main_slider');
+    var hamburger = $('.hamburger_container');
+    var menu = $('.hamburger_menu');
+    var menuActivate = false;
+    var hamburgerClose = $('.hamburger_close');
+    var fsOverlay = $('.fs_menu_overlay');
 
-// Table of contyent
-// 1.  vars and inits
-// 2.  Inits Menu
-// 3.  Init Timer
-// 4.  Init Favorite
-// 5.  Init Isotope Filtering
-// 6.  Init Slider
+    initFavorite();
 
 
+
+    //favorite
+
+    function initFavorite() {
+        if ($('.favorite').length) {
+            var favs = $('.favorite');
+            favs.each(function() {
+                var fav = $(this);
+                var active = false;
+                if (fav.hasClass('active')) {
+                    active = true;
+                }
+                fav.on('click', function() {
+                    if (active) {
+                        fav.removeClass('active');
+                        active = false;
+                    } else {
+                        fav.addClass('active');
+                        active = true;
+                    }
+                })
+            })
+        }
+    }
+})
 
