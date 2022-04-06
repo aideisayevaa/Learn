8fad59ef747bda09d777bfbafb5a545f2ba892a5
aideisayevaa <aideisayeva3@gmail.com>
2022-04-06 11:17:53 +0400

    hamburger icon tamamlandi

--- E-Commerce Website/js/custom.js
+++ E-Commerce Website/js/custom.js
@@ -1,10 +1,10 @@
-jQuery(document).ready(function ($) {
+jQuery(document).ready(function($) {
     "user strict";
 
     var mainSlider = $('.main_slider');
     var hamburger = $('.hamburger_container');
     var menu = $('.hamburger_menu');
-    var menuActivate = false;
+    var menuActive = false;
     var hamburgerClose = $('.hamburger_close');
     var fsOverlay = $('.fs_menu_overlay');
 
@@ -18,45 +18,56 @@ jQuery(document).ready(function ($) {
     //menu
     function initMenu() {
         if (hamburger.length) {
-            hamburger.on('click', function () {
+            hamburger.on('click', function() {
                 if (!menuActive) {
                     openMenu()
                 }
             })
         }
         if (fsOverlay.length) {
-            fsOverlay.on('click', function () {
+            fsOverlay.on('click', function() {
                 if (menuActive) {
                     closeMenu()
                 }
             })
         }
         if (hamburgerClose.length) {
-            hamburgerClose.on('click', function () {
+            hamburgerClose.on('click', function() {
                 if (!menuActive) {
                     closeMenu()
                 }
             })
         }
         if ($('.menu_item').length) {
-            var items = document.getElementsByClassName('.menu_item')
+            var items = document.getElementsByClassName('menu_item')
             var i
             for (i = 0; i < items.length; i++) {
                 if (items[i].classList.contains('has-children')) {
-                    items[i].onclick = function () {
+                    items[i].onclick = function() {
                         this.classList.toggle("active")
                         var panel = this.children[1]
-                        if (panel.style.maxHeight){
-                            panel.style.maxHeight=null
-                        }
-                        else{
-                            panel.style.maxHeight=panel.scrollHeight+"px"
+                        if (panel.style.maxHeight) {
+                            panel.style.maxHeight = null
+                        } else {
+                            panel.style.maxHeight = panel.scrollHeight + "px"
                         }
                     }
                 }
             }
         }
 
+    }
+
+    function openMenu() {
+        menu.addClass('active')
+        fsOverlay.css('pointer-events', "auto")
+        menuActive = true
+    }
+
+    function closeMenu() {
+        menu.removeClass('active')
+        fsOverlay.css('pointer-events', "none")
+        menuActive = false
     }
 
 
@@ -66,13 +77,13 @@ jQuery(document).ready(function ($) {
     function initFavorite() {
         if ($('.favorite').length) {
             var favs = $('.favorite');
-            favs.each(function () {
+            favs.each(function() {
                 var fav = $(this);
                 var active = false;
                 if (fav.hasClass('active')) {
                     active = true;
                 }
-                fav.on('click', function () {
+                fav.on('click', function() {
                     if (active) {
                         fav.removeClass('active');
                         active = false;
@@ -90,7 +101,7 @@ jQuery(document).ready(function ($) {
 
     function initIsotopeFiltering() {
         if ($('.grid_sorting_button').length) {
-            $('.grid_sorting_button').click(function () {
+            $('.grid_sorting_button').click(function() {
                 $('.grid_sorting_button.active').removeClass('active');
                 $(this).addClass('active');
 
@@ -132,7 +143,7 @@ jQuery(document).ready(function ($) {
             var s = $('#second');
 
 
-            setInterval(function () {
+            setInterval(function() {
                 var current_date = new Date().getTime();
                 var seconds_left = (target_date = current_date) / 1000;
 
@@ -190,12 +201,12 @@ jQuery(document).ready(function ($) {
                 }
             })
             if ($('.product_slider_nav_left').length) {
-                $('.product_slider_nav_left').on('click', function () {
+                $('.product_slider_nav_left').on('click', function() {
                     slider1.trigger('prev.owl.carousel')
                 })
             }
             if ($('.product_slider_nav_right').length) {
-                $('.product_slider_nav_right').on('click', function () {
+                $('.product_slider_nav_right').on('click', function() {
                     slider1.trigger('next.owl.carousel')
                 })
             }
