6258e17eef9aeaf29925fc7e38cf5ae4729c7bbc
aideisayevaa <aideisayeva3@gmail.com>
2022-04-10 17:59:08 +0400

    react - ders 2 - html ve css kodlari elave edildi

--- React/youtube/ders2/src/App.js
+++ React/youtube/ders2/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+      <>
+      <nav>
+        <h1>LOGO</h1>
+        <ul>
+          <li>main page</li>
+          <li>projects</li>
+          <li>about us</li>
+        </ul>
+      </nav>
+
+      <section className="bg-image"></section>
+    </>
+    );
 }
 
 export default App;
