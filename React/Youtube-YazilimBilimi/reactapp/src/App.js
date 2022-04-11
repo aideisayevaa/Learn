81f9482004b22fb6f013bd593aa512fa217006f7
aideisayevaa <aideisayeva3@gmail.com>
2022-04-11 09:17:37 +0400

    react - ders 8

--- React/Youtube-YazilimBilimi/reactapp/src/App.js
+++ React/Youtube-YazilimBilimi/reactapp/src/App.js
@@ -1,20 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
+import User from './components/User'
 
 function App() {
-  const test =34
-  const isAuth=true
   return (
     <div className='App'>
-      <h4>{1 + 1}</h4>
-      <h4>{"aida".toUpperCase()}</h4>
-      <h4>{test}</h4>
-      <div>
-        {
-          isAuth ? <p>Qeydiyyata alindi</p>
-                 : null
-        }
-      </div>
+     <h1>ADD COMPONENT</h1>
+      <User/>
+      <User/>
     </div>
   );
 }
---
+++ React/Youtube-YazilimBilimi/reactapp/src/components/User.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+
+class User extends Component {
+  render() {
+    return (
+      <div>
+          <form>
+              <input type="text" />
+              <button>Gonder</button>
+          </form>
+      </div>
+    )
+  }
+}
+
+
+
+export default User;
