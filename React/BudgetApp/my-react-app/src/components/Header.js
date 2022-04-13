dd25a82af505c84c9d75aaafa00483802ce8def7
aideisayevaa <aideisayeva3@gmail.com>
2022-04-13 10:24:29 +0400

    First React app - fayllar elave edildi

---
+++ React/BudgetApp/my-react-app/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+//import { useState } from 'react' - error cixsa yuxaridaki kod setri evezine bu olacaq
+
+function Header(props) {
+    return (
+        <>
+
+            header   
+            
+        </>
+    )
+}
+
+export default Header
