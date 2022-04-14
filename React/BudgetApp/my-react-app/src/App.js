02b15dc246174485641696f41d16f459d28b431c
aideisayevaa <aideisayeva3@gmail.com>
2022-04-14 09:40:59 +0400

    Udemy React - 3

--- React/BudgetApp/my-react-app/src/App.js
+++ React/BudgetApp/my-react-app/src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
+import { useState, useEffect } from 'react'
 import Header from './components/Header'
 
 function App() {
+
+  const [initialMoney, setInitialMoney]=useState(initialState: 100)
+
+
   return (
     <>
       <Header />
