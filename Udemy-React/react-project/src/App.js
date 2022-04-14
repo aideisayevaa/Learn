5967a8b3b34143454c0fc12c16c95a0fbbd94442
aideisayevaa <aideisayeva3@gmail.com>
2022-04-14 19:11:18 +0400

    Udemy React - 10

--- Udemy-React/react-project/src/App.js
+++ Udemy-React/react-project/src/App.js
@@ -3,7 +3,18 @@ import './App.css';
 function App() {
   return (
     <div>
-      Hello World
+      <a href="https://unsplash.com/">Hello world</a>
+
+      <img src="https://images.unsplash.com/photo-1649829875371-1f583c2cf057?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80" alt="" />
+    
+    <button>Button</button>
+    <p>p tagi</p>
+    <h1>Basliq 1</h1>
+    <h4>Basliq 2</h4>
+    <h3>Basliq 3</h3>
+
+    
+    
     </div>
   );
 }
