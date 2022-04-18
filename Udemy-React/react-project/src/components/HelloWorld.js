9e9bfb3a1baf82e8037115e155b2f08c5b2d1dca
aideisayevaa <aideisayeva3@gmail.com>
2022-04-18 11:49:05 +0400

    Udemy React - 16

--- Udemy-React/react-project/src/components/HelloWorld.js
+++ Udemy-React/react-project/src/components/HelloWorld.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import './HelloWorld.css'
 
-const HelloWorld = () => {
+const HelloWorld = (props) => {
+   
     return (
         <div className='helloWorldWrapper'>
-            <img className='helloWorldImage' src="https://images.unsplash.com/photo-1646920865283-7ad635281ad9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDJ8aFNQNkp4OHc0WjR8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60" alt="" />
-            <button className='helloWorldButton'>Button</button>
-            <p className="hellWorldP">Lorem ipsum dolor sit amet.</p>
+            <img className='helloWorldImage' src={props.resim} alt="" />
+            <button className='helloWorldButton'>{props.button}</button>
+            <p className="hellWorldP">{props.yazi}</p>
+
+
+
+            <div className="helloWorldRedArea">
+                {props.children}
+            </div>
         </div>
     )
 }
