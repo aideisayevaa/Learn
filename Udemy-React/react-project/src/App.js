c5c53d555125089d0ef69f3be61d885f4ccaa670
aideisayevaa <aideisayeva3@gmail.com>
2022-05-15 19:41:40 +0400

    Udemy React - 26(3)

--- Udemy-React/react-project/src/App.js
+++ Udemy-React/react-project/src/App.js
@@ -3,45 +3,26 @@ import './App.css';
 
 
 function App() {
+  const onClickLi = (name) => {
+    console.log(name)
+  }
 
-  const [count, setCount] = useState(30)
-
-  const [message, setMessage] = useState("Hello")
-
-  const [isActive, setIsActive] = useState(false)
-
+  const [data, setData] = useState([{ name: "Aida" }, { name: "aa" }, { name: "Aida2" }, { name: "Aida3" }, { name: "Aida" }])
   return (
-
-    <div>
-
-      <h1>{count}</h1>
-
-      <button onClick={() => {
-        setCount(count - 1)
-      }}>Azalt</button>
-
-
-      <button onClick={() => {
-        setCount(count + 1)
-      }}>Artir</button>
-
-
-      <p>{message}</p>
-
-      <button onClick={() => {
-        setMessage("Metn deyisdirildi")
-      }}>Metni deyis</button>
-
-
-
-      <h4>{JSON.stringify(isActive)}</h4>
-
-      <input type="checkbox" checked={isActive} onChange={(e)=>{
-        setIsActive(e.target.checked)
-      }} />
-
-    </div>
-  );
+    <div className="App">
+      <h1>List</h1>
+      {data.map((item, index) => {
+        if (item.name.length >= 3) {
+          return <h1 key={index}>{item.name}</h1>
+        }
+        else {
+          return <p key={index} onClick={() => {
+            onClickLi(item.name)
+          }}>{item.name.length <= 2 ? "Addaki herf sayi azdi" : item.name}</p>
+        }
+      })}
+    </div >
+  )
 }
 
 export default App;
---
+++ Udemy-React/react-project/src/Components/HellloWorld.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function HellolWorld() {
+    return (
+        <h6>HELLO WORLD!</h6>
+    )
+}
+
+export default HellolWorld
