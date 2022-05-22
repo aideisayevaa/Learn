f4115fd7788c8bc7119fa58cf6c8e23e4b33bb11
aideisayevaa <aideisayeva3@gmail.com>
2022-05-22 22:07:06 +0400

    Udemy React - 32

--- Udemy-React/react-project/src/Components/card/card.js
+++ Udemy-React/react-project/src/Components/card/card.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import './card.css'
+import CardTitle from '../cardTitle/cardTitle'
 
 const Card = () => {
     return (
        <div className="card">
-           <div className="card-body">card</div>
+           <div className="card-body">
+               <CardTitle title={"Men Kart basligiyam"}/>
+           </div>
        </div>
 
     )
---
+++ Udemy-React/react-project/src/Components/cardTitle/cardTitle.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './cardTitle.css'
+
+const CardTitle = ({ title }) => {
+    return (
+        <div className="card-title">{title}</div>
+    )
+}
+
+
+export default CardTitle
