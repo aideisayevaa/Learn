ad54087b088f9ac5d6427294a059526826ed4b9a
aideisayevaa <aideisayeva3@gmail.com>
2022-05-26 15:57:06 +0400

    Udemy React - 42

--- Udemy-React/react-project/src/App.js
+++ Udemy-React/react-project/src/App.js
@@ -1,32 +1,14 @@
-import React, { useState } from 'react'
+import React from 'react'
 import './App.css';
-import styles from "./CustomStyle.module.css"
+import Test from './components/test';
 
 function App() {
 
-  const [data, setData] = useState([
-    {
-      text: 'Yasin',
-      status: "success"
-    },
-    {
-      text: 'Ali',
-      status: 'error'
-    },
-    {
-      text: 'Mehmet',
-      status: 'primary'
-    }])
 
   return (
     <div>
-      {data.map((item, index) => {
-        return (
-          <div key={index} className={styles[item.status]}>
-            {item.text}
-          </div>
-        )
-      })}
+      hello
+     <Test text="merhaba dunya"/>
     </div>
   )
 }
---
+++ Udemy-React/react-project/src/components/test.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Test = ({ text, text2 }) => {
+    return (
+        <>
+            <div>{text}</div>
+            <div>{text2}</div>
+        </>
+
+    )
+}
+
+export default Test
